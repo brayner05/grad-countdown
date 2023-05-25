85dd6c15b82744a9970c2d60001acb90d436bf80
bcer-dev <braydenrayner@outlook.com>
2023-05-24 23:13:56 -0300

    Redid UI

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ var GRADUATION = new Date("2023-06-22");
  * Get the number of days until GRADUATION.
  * @returns The number of days until graduation
  */
-function calculateRemainingDays() {
+var calculateRemainingDays = function () {
     return ((GRADUATION.getTime() - Date.now()) / (3600000 * 24));
-}
+};
 /**
  * Displays the number of days until graduation.
  * @param counter A reference to the counter element.
  */
-function displayRemainingDays(counter) {
+var displayRemainingDays = function (counter) {
     var days = calculateRemainingDays();
     if (days > 0 && days < 1) {
         counter.textContent = "Today is graduation!";
@@ -22,9 +22,21 @@ function displayRemainingDays(counter) {
     else {
         counter.textContent = "Graduation has already happened!";
     }
-}
+};
+var getCurrentUrl = function () {
+    return window.location.href;
+};
+var handleShare = function (shareBtn) {
+    var url = getCurrentUrl();
+    navigator.clipboard.writeText("Countdown to graduation with me at " + url + "!");
+    shareBtn.innerText = "Copied to Clipboard!";
+};
 // Display the number of days
 var dayCounter = document.querySelector("#day-counter");
 if (dayCounter) {
     displayRemainingDays(dayCounter);
+}
+var shareButton = document.getElementById("share");
+if (shareButton) {
+    shareButton.addEventListener("click", function () { return handleShare(shareButton); });
 }
--- script.ts
+++ script.ts
@@ -5,7 +5,7 @@ const GRADUATION = new Date("2023-06-22");
  * Get the number of days until GRADUATION.
  * @returns The number of days until graduation
  */
-function calculateRemainingDays() : number {
+const calculateRemainingDays = () : number => {
     return ((GRADUATION.getTime() - Date.now()) / (3600000 * 24));
 }
 
@@ -13,7 +13,7 @@ function calculateRemainingDays() : number {
  * Displays the number of days until graduation.
  * @param counter A reference to the counter element.
  */
-function displayRemainingDays(counter: HTMLHeadingElement) : void {
+const displayRemainingDays = (counter: HTMLHeadingElement) : void => {
     const days = calculateRemainingDays();
     if (days > 0 && days < 1) {
         counter.textContent = "Today is graduation!";
@@ -26,9 +26,23 @@ function displayRemainingDays(counter: HTMLHeadingElement) : void {
     }
 }
 
+const getCurrentUrl = (): string => {
+    return window.location.href;
+}
+
+const handleShare = (shareBtn: HTMLElement) => {
+    const url = getCurrentUrl();
+    navigator.clipboard.writeText(`Countdown to graduation with me at ${url}!`);
+    shareBtn.innerText = "Copied to Clipboard!";
+}
+
 // Display the number of days
 const dayCounter: HTMLHeadingElement | null = document.querySelector("#day-counter");
 if (dayCounter) {
     displayRemainingDays(dayCounter);
 }
 
+const shareButton = document.getElementById("share");
+if (shareButton) {
+    shareButton.addEventListener("click", () => handleShare(shareButton));
+}
